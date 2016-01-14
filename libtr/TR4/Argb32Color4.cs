177bf6d3b03383442e58e89f2796086498600a7c
Splitty <splittydev@gmail.com>
2016-01-14 05:08:14 +0100

    Added TR4 Argb32Color4 data structure

---
+++ libtr/TR4/Argb32Color4.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace libtr.TR4
+{
+	/* Layout (ARGB MSB-to-LSB):
+	* 8-bit transparency
+	* 8-bit red channel
+	* 8-bit green channel
+	* 8-bit blue channel
+	*/
+
+	/// <summary>
+	/// TR4 Encoded 32-Bit Color with Transparency
+	/// </summary>
+	[StructLayout (LayoutKind.Explicit, Size = 4)]
+	public struct Argb32Color4
+	{
+		/// <summary>
+		/// The value.
+		/// </summary>
+		[FieldOffset (0)]
+		readonly public UInt32 Value;
+
+		/// <summary>
+		/// The alpha component
+		/// </summary>
+		[FieldOffset (0)]
+		readonly public Byte A;
+
+		/// <summary>
+		/// The red component
+		/// </summary>
+		[FieldOffset (1)]
+		readonly public Byte R;
+
+		/// <summary>
+		/// The green component
+		/// </summary>
+		[FieldOffset (2)]
+		readonly public Byte G;
+
+		/// <summary>
+		/// The blue component
+		/// </summary>
+		[FieldOffset (3)]
+		readonly public Byte B;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="libtr.TR4.Argb32Color4"/> struct.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		public Argb32Color4 (UInt32 value) : this () {
+			Value = value;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="libtr.TR4.Argb32Color4"/> struct.
+		/// </summary>
+		/// <param name="a">The alpha component.</param>
+		/// <param name="r">The red component.</param>
+		/// <param name="g">The green component.</param>
+		/// <param name="b">The blue component.</param>
+		public Argb32Color4 (Byte a, Byte r, Byte g, Byte b) : this () {
+			A = a;
+			R = r;
+			G = g;
+			B = b;
+		}
+	}
+}
+
---
+++ libtr/TR4/TexTile32.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace libtr.TR4
+{
+	/// <summary>
+	/// TR4 32-Bit Texture Tile
+	/// </summary>
+	[StructLayout (LayoutKind.Explicit, Size = 262144)]
+	public unsafe struct TexTile32
+	{
+		/// <summary>
+		/// The tiles
+		/// </summary>
+		[FieldOffset (0)]
+		public fixed UInt32 Tiles [65536]; // 262144 bytes (256 * 256 * 4)
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="libtr.TR4.TexTile32"/> struct.
+		/// </summary>
+		/// <param name="tiles">The tiles.</param>
+		public TexTile32 (UInt32 [] tiles) : this () {
+			fixed (UInt32 *ptr = Tiles) {
+				for (var i = 0; i < Math.Min (tiles.Length, 65536); i++)
+					*(ptr + i) = tiles [i];
+			}
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="libtr.TR4.TexTile32"/> struct.
+		/// </summary>
+		/// <param name="tiles">The tiles.</param>
+		public TexTile32 (Argb32Color4 [] tiles) : this () {
+			fixed (UInt32 *ptr = Tiles) {
+				for (var i = 0; i < Math.Min (tiles.Length, 65536); i++)
+					*(ptr + i) = tiles [i].Value;
+			}
+		}
+	}
+}
+
