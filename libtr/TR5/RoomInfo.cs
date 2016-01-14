d3b400a4c46043b17a50e8aacdb218e5e1bc5012
Splitty <splittydev@gmail.com>
2016-01-14 11:31:47 +0100

    Added TR5 Room Info data structure

--- libtr/TR5/RoomInfo.cs
+++ libtr/TR5/RoomInfo.cs
@@ -1,47 +1,54 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace libtr
+namespace libtr.TR5
 {
 	/// <summary>
-	/// TR Room Info
+	/// TR5 Room Info
 	/// </summary>
-	[StructLayout (LayoutKind.Explicit, Size = 16)]
+	[StructLayout (LayoutKind.Explicit, Size = 20)]
 	public struct RoomInfo
 	{
 		/// <summary>
-		/// X coordinate
+		/// X offset in world coordinates
 		/// </summary>
 		[FieldOffset (0)]
 		readonly public Int32 X;
 
 		/// <summary>
-		/// Z coordinate
+		/// Y offset in world coordinates
 		/// </summary>
 		[FieldOffset (4)]
+		readonly public Int32 Y;
+
+		/// <summary>
+		/// Z offset in world coordinates
+		/// </summary>
+		[FieldOffset (8)]
 		readonly public Int32 Z;
 
 		/// <summary>
 		/// Bottom Y coordinate
 		/// </summary>
-		[FieldOffset (8)]
+		[FieldOffset (12)]
 		readonly public Int32 YBottom;
 
 		/// <summary>
 		/// Top Y coordinate
 		/// </summary>
-		[FieldOffset (12)]
+		[FieldOffset (16)]
 		readonly public Int32 YTop;
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="libtr.RoomInfo"/> struct.
+		/// Initializes a new instance of the <see cref="libtr.TR5.RoomInfo"/> struct.
 		/// </summary>
 		/// <param name="x">The x coordinate.</param>
 		/// <param name="z">The z coordinate.</param>
 		/// <param name="ybottom">The bottom Y coordinate.</param>
 		/// <param name="ytop">The top Y coordinate.</param>
-		public RoomInfo (Int32 x, Int32 z, Int32 ybottom, Int32 ytop) : this () {
+		public RoomInfo (Int32 x, Int32 y, Int32 z, Int32 ybottom, Int32 ytop) : this () {
 			X = x;
+			Y = y;
 			Z = z;
 			YBottom = ybottom;
 			YTop = ytop;
